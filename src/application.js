df2118d7ee6e491f14ddd8818971aebe6dc7ca8e
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-02-08 14:41:54 +0100

    Start coordinate suggestions

--- src/application.js
+++ src/application.js
@@ -58,26 +58,98 @@ export class Application {
   async run () {
     // Scrape start page for links to calendar, cinema and restaurant.
     const startPage = new StartPage(this.#url)
+    process.stdout.write('Scraping links...')
     const links = await startPage.getLinks()
+    process.stdout.write('OK\n')
 
     // Scrape calendar page for links to Paul, Peter and Mary, and scrape their pages for free days.
     const calendar = new Calendar(links.calendar)
+    process.stdout.write('Scraping available days...')
     const freeDays = await calendar.getFreeDaysAll()
+    process.stdout.write('OK\n')
 
     // Start two asyncronus tasks (check movies, check free tables - for the day(s) when all friends can meet).
 
-    // Check movies
+    // Check movies.
     const cinema = new Cinema(links.cinema)
+    process.stdout.write('Scraping showtimes...')
     const moviesPromises = freeDays.map(day => cinema.getMovies(day))
+    process.stdout.write('OK\n')
 
-    const movies = await Promise.all([...moviesPromises]) // Glöm ej att ta bort detta!
-
-    // Check free tables
+    // Check free tables.
     const dinner = new Dinner(links.dinner)
-    const freeTablesPromises = freeDays.map(day => dinner.getFreeTables(day)) // FORTSÄTT HÄR!!!
+    process.stdout.write('Scraping possible reservations...')
+    const freeTablesPromises = freeDays.map(day => dinner.getFreeTables(day))
+    process.stdout.write('OK\n')
 
-    const freeTables = await Promise.all([...freeTablesPromises]) // Glöm ej att ta bort detta!
+    // Resolve moviesPromises and freeTablesPromises.
+    const movies = await Promise.all([...moviesPromises])
+    const freeTables = await Promise.all([...freeTablesPromises])
 
-    // Resolve moviesPromises and freeTablesPromises
+    // FORTSÄTT HÄR!!!
+    // Jämför freeDays, movies och freeTables
+    const suggestions = this.#getSuggestions(freeDays, movies, freeTables)
+    this.#printSuggestions(suggestions)
+  }
+
+  #getSuggestions (freeDays, movies, freeTables) {
+    const suggestions = []
+    let day, movieTitle, movieTime, table
+    // For every free day.
+    for (let i = 0; i < freeDays.length; i++) {
+      day = freeDays[i]
+      // For every movie that day.
+      for (let j = 0; j < movies[i].length; j++) {
+        movieTitle = movies[i][j].movie
+        // For every available show for that movie that day.
+        for (let k = 0; k < movies[i][j].shows.length; k++) {
+          movieTime = movies[i][j].shows[k]
+          // For every free table that day.
+          for (let l = 0; l < freeTables[i].length; l++) {
+            table = freeTables[i][l]
+            // If the table is free at least 2 hours after the show...
+            if (this.#compareTableToShow(table, movieTime)) {
+              // ...push a suggestion to the array 'suggestions'.
+              suggestions.push({
+                day: day,
+                movie: {
+                  title: movieTitle,
+                  time: movieTime
+                },
+                table: table
+              })
+            }
+          }
+        }
+      }
+    }
+    return suggestions
+  }
+
+  /**
+   * 
+   * @param {*} table 
+   * @param {*} show 
+   * @returns 
+   */
+  #compareTableToShow (table, show) {
+    const tableInt = parseInt(table.slice(0, 2))
+    const showInt = parseInt(show.slice(0, 2))
+
+    if (tableInt - showInt >= 2) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  #printSuggestions (suggestions) {
+    const suggestionsText = this.#turnSuggestionsIntoText(suggestions)
+    console.log('Suggestions\n===========')
+    suggestionsText.forEach(suggestion => console.log(suggestion))
+  }
+
+  #turnSuggestionsIntoText (suggestions) {
+    return suggestions.map(suggestion => `* On ${suggestion.day}, "${suggestion.movie.title}" begins at ${suggestion.movie.time}, and there is a free table to book between ${suggestion.table}.`)
   }
 }
